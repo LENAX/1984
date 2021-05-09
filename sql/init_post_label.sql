cb3d8dd338bbb5261ccb88246eae85034135f22b
stevelan <stevelan1995@outlook.com>
2021-05-09 16:55:00 +0800

    added data and initialize db

---
+++ sql/init_post_label.sql
@@ -0,0 +1,39 @@
+-- 政治敏感类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (1, 'politics', 1);
+
+-- 暴恐类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (2, 'violence', 2);
+
+-- 色情类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (3, 'porn', 3);
+
+-- 广告类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (4, 'spam', 4);
+
+-- 赌博类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (5, 'gamble', 5);
+
+-- 违法犯罪类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (6, 'crime', 6);
+
+-- 突发事件类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (7, 'incident', 7);
+
+-- 辱骂类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (8, 'curse', 8);
+
+-- 其它类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (9, 'other', 9);
+
+-- 正常内容类
+INSERT into POST_CATEGORY (id, post_category_name, post_category_label)
+VALUES (10, 'normal', 10);
